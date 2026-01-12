39ec2fcf40850d7f706157fbb0de0512df32f01e
SmallWhite <hi.hi.terry123@gmail.com>
2026-01-12 23:01:35 +0800

    preparing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+# WIP
+
+
 # Daily-Red-Quote
 A small tool to enhance patriotism education of People's Republic of China
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+import os
 
 class UI(tk.Tk):
     def __init__(self):
@@ -10,7 +12,6 @@ class UI(tk.Tk):
         self.main_page()
         
     def init_screen(self):
-        self.update_idletasks()
         w = 360
         h = 360
         x = (self.winfo_screenwidth() // 2) - (w // 2)
@@ -18,20 +19,36 @@ class UI(tk.Tk):
         self.geometry(f"{w}x{h}+{x}+{y}")
         
     def main_page(self):
+        style_title = {
+            "bg": "red",
+            "fg": "gold",
+            "font": ("", 32, "bold"),}
+        style_subtitle = {
+            "bg": "red",
+            "fg": "gold",
+            "font": ("", 12, "bold"),}
         style_common = {
             "bg": "gold",
-            "activebackground": "orange",
-            "relief": "groove",
-            "bd": 2,
-            "font": ("", 12, "bold"),}
-        box = tk.Frame(self, bg="red", bd=1, relief="solid")
-        box.pack(expand=True, fill="both")
-
-        btn1 = tk.Button(box, text="<每日一語>", **style_common)
-        btn1.grid(row=2, column=0, padx=5, pady=5, ipadx=16, ipady=8)
-
-        btn2 = tk.Button(box, text="<紅色電台>", **style_common)
-        btn2.grid(row=0, column=1, padx=5, pady=5, ipadx=16, ipady=8)
+            "font": ("", 16, "bold"),}
+        tk.Label(self, text="=红语录=", **style_title).pack(pady=10)
+        
+        ## ICON
+        img = Image.open(os.path.join(os.getcwd(), "data", "mainpage_icon.png"))
+        img = img.resize((160, 160))
+        self.icon_img = ImageTk.PhotoImage(img)
+        tk.Label(self, image=self.icon_img, bg="red").pack(pady=5)
+        
+        ## OPTIONS
+        options_frame = tk.Frame(self, bg="red")
+        of1 = tk.Frame(options_frame, bg="red")
+        tk.Button(of1, text="<每日一语>", **style_common).pack(side="left", padx=10)
+        tk.Button(of1, text="<红色电台>", **style_common).pack(side="left", padx=10)
+        of1.pack(pady=5)
+        
+        of2 = tk.Frame(options_frame, bg="red")
+        tk.Label(of2, text="由 小白SmallWhite 整理", **style_subtitle).pack(side="left", padx=10)
+        of2.pack(side="left", pady=5)
+        options_frame.pack(side="top", pady=10)
 
 if __name__ == "__main__":
     app = UI()
